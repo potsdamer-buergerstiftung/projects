71c197ff88fa94c81d34ad91f4515cdf4c6ed1ee
Nöl <65312133+linlogge@users.noreply.github.com>
2021-12-15 13:42:12 +0000

    disable map

--- apps/website/components/contact/Map.tsx
+++ apps/website/components/contact/Map.tsx
@@ -7,7 +7,6 @@ const Map: FC = () => {
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={position}>
--- apps/website/pages/kontakt.tsx
+++ apps/website/pages/kontakt.tsx
@@ -1,14 +1,8 @@
 import { Default } from "@components/layouts";
-import dynamic from "next/dynamic";
-
-const DynamicComponent = dynamic(() => import("@components/contact/Map"), {
-  ssr: false,
-});
 
 export default function ContactPage() {
   return (
     <>
-      <DynamicComponent />
     </>
   );
 }
