971b3a449b88f412efec32bcec0b61b71f787400
Nöl <65312133+linlogge@users.noreply.github.com>
2022-04-20 12:09:57 +0200

    add redirects

--- apps/links/api/7znkqa.ts
+++ apps/links/api/7znkqa.ts
@@ -1,5 +1,7 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
+// Drachenboot-Link-Spenden
+
 export default function (req: VercelRequest, res: VercelResponse) {
-  res.redirect(301, "https://vercel.com/docs/functions/http");
+  res.redirect(301, "https://www.potsdam-crowd.de/drachenboot");
 }
