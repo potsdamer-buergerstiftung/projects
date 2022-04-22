943776f1b46653bd38295e9d00d7f711959af275
Nöl <65312133+linlogge@users.noreply.github.com>
2022-04-22 14:06:02 +0200

    change analytics url script

--- apps/website/components/core/Head.tsx
+++ apps/website/components/core/Head.tsx
@@ -15,7 +15,7 @@ const Head: FC = () => {
             defer
             data-website-id="1b0d1b14-b2bd-4d55-bba7-103f92c44166"
             src="https://analytics.potsdamer-buergerstiftung.org/umami.js"
-          />
+          ></script>
         )}
       </NextHead>
     </>
