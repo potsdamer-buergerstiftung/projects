80520acea44863c2801f8f399080bebccb856de6
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-06 10:37:44 +0200

    add html lang attr

--- apps/inselbuehne/nuxt.config.ts
+++ apps/inselbuehne/nuxt.config.ts
@@ -2,6 +2,13 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: "de",
+      },
+    },
+  },
   modules: ["@nuxtjs/tailwindcss"],
   css: ["@fontsource/dm-sans", "@fontsource/dm-serif-display"],
 });
