605463967bc734f8cc76a760c218f9c1f4fddd2e
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-10 22:52:15 +0200

    use css only for hover animation

--- apps/pbswebsite/components/project/card/ProjectCard.vue
+++ apps/pbswebsite/components/project/card/ProjectCard.vue
@@ -7,20 +7,20 @@ function setHovered(value: boolean) {
 </script>
 
 <template>
-    <div class="w-full h-96 relative overflow-hidden rounded-lg cursor-pointer" @mouseenter="setHovered(true)"
+    <div class="group w-full h-96 relative overflow-hidden rounded-lg cursor-pointer" @mouseenter="setHovered(true)"
         @mouseleave="setHovered(false)">
-        <!-- <nuxt-img preset="cover" src="/img/img.jpg" /> -->
+        <img src="~/assets/img/img.jpg" class="object-cover transition duration-500 group-hover:scale-110" />
         <div
             class="absolute top-0 right-0 left-0 bottom-0 pointer-events-none bg-gradient-to-t from-black opacity-60" />
         <div class="absolute flex flex-column items-end top-0 right-0 left-0 bottom-0 p-8">
             <div class="relative w-full">
                 <h1 class="text-white text-2xl font-bold">Projekt</h1>
                 <div class="relative overflow-hidden w-full mt-2">
-                    <p class="text-white block transition duration-300"
-                        :class="{ 'translate-y-0 opacity-1': !isHovered, '-translate-y-5 opacity-0': isHovered }">
+                    <p
+                        class="text-white block transition duration-500 translate-y-0 opacity-100 group-hover:-translate-y-3 group-hover:opacity-0">
                         Projektbeschreibung</p>
-                    <div class="text-white absolute transition duration-300"
-                        :class="{ 'translate-y-0 opacity-0': !isHovered, '-translate-y-full opacity-1': isHovered }">
+                    <div
+                        class="text-white absolute transition duration-500 translate-y-0 opacity-0 group-hover:-translate-y-full group-hover:opacity-100">
                         <NuxtLink to="/" class="relative font-bold">
                             Projekt anzeigen
                         </NuxtLink>
