057577481037ca26a7125b102ee04ef5b71b8caf
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-23 20:15:48 +0200

    add analytics

--- apps/inselbuehne/layouts/default.vue
+++ apps/inselbuehne/layouts/default.vue
@@ -11,6 +11,8 @@ useHead({
 
 <template>
     <div class="antialiased">
+        <Script async defer data-website-id="54874799-7dc4-407f-8ee6-ea2d08dc4cf1"
+            src="https://analytics.potsdamer-buergerstiftung.org/umami.js"></Script>
         <Title>{{ route.meta.title }}</Title>
         <Header />
         <slot />
