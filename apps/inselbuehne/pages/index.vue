df502b6dedf1714041f204e98603a38529e82c5a
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-23 21:30:27 +0200

    add image

--- apps/inselbuehne/pages/index.vue
+++ apps/inselbuehne/pages/index.vue
@@ -13,7 +13,7 @@ const posts = await getItems({ collection: "posts" })
 <template>
     <div>
         <section class="bg-green-100">
-            <div class="container items-center px-4 pt-48 pb-24 mx-auto grid grid-cols-6 gap-16">
+            <div class="container grid items-center grid-cols-6 gap-16 px-4 pt-48 pb-24 mx-auto">
                 <div class="max-w-xl col-span-6 md:col-span-3">
                     <h1 class="font-serif text-5xl lg:text-6xl">Wir gehen in die <span class="text-green-500">zweite
                             Runde</span>.
@@ -23,12 +23,12 @@ const posts = await getItems({ collection: "posts" })
                         Verfügung zu stellen, der letztes Jahr tausende Zuschauerinnen und Zuschauer begeistern konnte.
                     </p>
                     <NuxtLink to="/veranstaltungen"
-                        class="inline-flex px-5 py-3 mt-6 text-lg font-medium text-white bg-green-500 rounded-tl-lg rounded-br-lg shadow-md transition hover:bg-green-400">
+                        class="inline-flex px-5 py-3 mt-6 text-lg font-medium text-white transition bg-green-500 rounded-tl-lg rounded-br-lg shadow-md hover:bg-green-400">
                         Zu den Veranstaltungen
                     </NuxtLink>
                 </div>
                 <div class="col-span-6 md:col-span-3">
-                    <div class="aspect-[5/3]">
+                    <div class="aspect-[5/3] bg-green-500 bg-cover">
                         <img :src="`https://cms.potsdamer-buergerstiftung.org/assets/a5ff92e4-a012-45b6-bc5d-352b219dcdf6?key=low-1000`"
                             alt="test" class="object-cover w-full h-full">
                     </div>
@@ -37,7 +37,7 @@ const posts = await getItems({ collection: "posts" })
         </section>
         <section class="bg-gray-50">
             <div class="container px-4 pt-16 pb-8 mx-auto">
-                <div class="flex flex-col justify-between md:flex-row md:items-center gap-4">
+                <div class="flex flex-col justify-between gap-4 md:flex-row md:items-center">
                     <div>
                         <hr class="w-16 h-1 bg-green-500" />
                         <h1 class="pt-5 font-serif text-3xl md:text-4xl">
@@ -52,7 +52,7 @@ const posts = await getItems({ collection: "posts" })
                         </button>
                     </div>
                 </div>
-                <div class="mt-12 grid grid-cols-6 gap-8">
+                <div class="grid grid-cols-6 gap-8 mt-12">
                     <div class="col-span-6 md:col-span-3 lg:col-span-2">
                         <ArticleCard v-for="post in posts" :post-id="post.id" :image-url="post.image"
                             :title="post.title" :summary="post.excerpt" />
@@ -61,10 +61,11 @@ const posts = await getItems({ collection: "posts" })
             </div>
         </section>
         <section class="bg-gray-50">
-            <div class="container items-center px-4 pt-16 mx-auto grid grid-cols-2 gap-16">
+            <div class="container grid items-center grid-cols-2 gap-16 px-4 pt-16 mx-auto">
                 <div class="order-2 col-span-2 lg:col-span-1 lg:order-1">
                     <div class="aspect-[3/2] bg-green-500 bg-cover">
-
+                        <img :src="`https://cms.potsdamer-buergerstiftung.org/assets/aa4cc52e-5b9d-4ede-b47f-820fcb729f2d?key=low-1000`"
+                            alt="test" class="object-cover w-full h-full">
                     </div>
                 </div>
                 <div class="order-1 col-span-2 lg:col-span-1 lg:order-2">
@@ -86,10 +87,13 @@ const posts = await getItems({ collection: "posts" })
                 <h4 class="text-sm font-bold tracking-widest text-green-500 uppercase">Los geht's</h4>
                 <h1 class="mt-3 font-serif text-4xl">Hol' dir dein Ticket</h1>
                 <p class="mt-4 text-lg leading-relaxed text-gray-600">
-                    Wegen aktueller Verordnungen dürfen wir nur 140 Personen mit entsprechendem Abstand zulassen. Um
-                    eine
-                    Veranstaltung zu besuchen, sind trotz der überwiegenden Mehrheit kostenloser Veranstaltungen Tickets
-                    notwendig, die Du entweder über unsere Website oder über unser Ticket-Telefon bestellen kannst.
+                    Um die Teilnehmerzahl verwalten zu können, sind <span class="font-bold">für das Besuchen</span>
+                    aller Veranstaltungen ab dem 15.
+                    Juni <span class="font-bold">Tickets notwendig</span>. Wir arbeiten zurzeit an einer Implementierung
+                    unseres neuen Ticket-Systems
+                    und werden dieses <span class="font-bold">zum 1. Juni</span> bereitstellen. Bis dahin können die
+                    Veranstaltungen auch ohne
+                    Registrierung besucht werden.
                 </p>
                 <NuxtLink to="/veranstaltungen"
                     class="inline-flex mt-4 shadow-md px-4 py-2.5 rounded-tl-lg rounded-br-lg bg-green-500 text-white text-md font-medium transition hover:bg-green-400">
--- apps/inselbuehne/pages/veranstaltungen/index.vue
+++ apps/inselbuehne/pages/veranstaltungen/index.vue
@@ -20,16 +20,18 @@ const posts = await getItems({
                 <h4 class="text-sm font-bold tracking-widest text-green-500 uppercase">Veranstaltungen</h4>
                 <h1 class="mt-6 font-serif text-5xl md:text-6xl">Bühne frei</h1>
                 <p class="pt-5 text-lg text-gray-600">
-                    Hier könnt ihr euch für unsere Veranstaltungen Tickets reservieren. Die Veranstaltungen sind
-                    kostenlos,
-                    wir möchten euch während der Bestellung dennoch um eine Spende bitten, um die Inselbühne sowohl als
-                    auch die Künstlerinnen und Künstler
-                    unterstützen zu können.
+                    Um die Teilnehmerzahl verwalten zu können, sind <span class="font-bold">für das Besuchen</span>
+                    aller Veranstaltungen ab dem 15.
+                    Juni <span class="font-bold">Tickets notwendig</span>. Wir arbeiten zurzeit an einer Implementierung
+                    unseres neuen Ticket-Systems
+                    und werden dieses <span class="font-bold">zum 1. Juni</span> bereitstellen. Bis dahin können die
+                    Veranstaltungen auch ohne
+                    Registrierung besucht werden.
                 </p>
             </div>
         </section>
         <section class="bg-gray-50">
-            <div class="container flex flex-col px-4 pb-32 mx-auto text-center space-y-10">
+            <div class="container flex flex-col px-4 pb-32 mx-auto space-y-10 text-center">
                 <EventCard v-for="event in posts" :title="event.title" :eventId="event.id" :summary="event.summary"
                     :start="event.start" :image="event.image" :registration_needed="event.registration_needed" />
             </div>
