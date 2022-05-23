8b517f5a1d4902062b88d008c9f07e3122683f03
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-23 20:56:49 +0200

    make open transition ease

--- apps/inselbuehne/components/header/nav/HeaderMobileNav.vue
+++ apps/inselbuehne/components/header/nav/HeaderMobileNav.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="mdAndSmaller"
-        class="fixed top-0 bottom-0 left-0 right-0 z-40 overflow-hidden bg-white overscroll-contain lg:hidden -translate-x-full transition duration-500"
+        class="fixed top-0 bottom-0 left-0 right-0 z-40 overflow-hidden bg-white transition duration-500 ease-in-out -translate-x-full overscroll-contain lg:hidden"
         :class="{ 'translate-x-0': headerState.isMobileMenuOpen }">
 
         <div class="container flex flex-row justify-between px-4 py-2 mx-auto mt-8">
