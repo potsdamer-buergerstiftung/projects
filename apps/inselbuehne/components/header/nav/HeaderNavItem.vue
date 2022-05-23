c5a25b0697d7dc7320bbdc72f13396eb48532c4a
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-23 14:29:23 +0200

    add active class

--- apps/inselbuehne/components/header/nav/HeaderNavItem.vue
+++ apps/inselbuehne/components/header/nav/HeaderNavItem.vue
@@ -9,7 +9,9 @@ const { title, link, isExternal = false } = defineProps<Props>()
 </script>
 
 <template>
-    <NuxtLink :to="link" class="p-2 text-lg">
+    <NuxtLink :to="link"
+        class="relative hover:after:opacity-100 p-2 text-lg after:bg-black after:absolute after:bottom-0 after:block after:h-[1.5px] after:right-6 after:left-6 after:opacity-0 after:transition"
+        active-class="after:opacity-100">
         {{ title }}
     </NuxtLink>
 </template>
