62dd7f77d08143a83431f00b5f7051acebb9590e
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-23 15:07:30 +0200

    add responsive mobile menu button

--- apps/inselbuehne/components/header/nav/HeaderNav.vue
+++ apps/inselbuehne/components/header/nav/HeaderNav.vue
@@ -1,12 +1,21 @@
 <template>
-    <nav class="flex flex-row items-center justify-center w-full h-full">
-        <div class="flex flex-row items-center justify-end w-full gap-x-5 h-full">
+    <nav
+        class="container mx-auto flex px-4 lg:px-0 flex-row items-center justify-between lg:justify-center w-full h-full">
+        <div class="hidden lg:flex flex-row items-center justify-end w-full gap-x-3 xl:gap-x-5 h-full">
             <HeaderNavItem title="Veranstaltungen" link="/veranstaltungen" />
             <HeaderNavItem title="Blog" link="/blog" />
             <HeaderNavItem title="Über uns" link="/ueber-uns" />
         </div>
         <HeaderNavBrand />
-        <div class="flex flex-row items-center justify-start w-full gap-x-5 h-full">
+        <div class="lg:hidden flex flex-row items-center">
+            <span class="font-bold">Menü</span>
+            <div class="flex flex-col space-y-1.5 ml-2">
+                <div class="h-[1px] w-6 bg-black" />
+                <div class="h-[1px] w-6 bg-black" />
+                <div class="h-[1px] w-6 bg-black" />
+            </div>
+        </div>
+        <div class="hidden lg:flex flex-row items-center justify-start w-full gap-x-5 h-full">
             <HeaderNavItem title="Presse" link="/presse" />
             <HeaderNavItem title="Unterstützen" link="/Unterstützen" />
             <HeaderNavItem title="Kontakt" link="/kontakt" />
--- apps/inselbuehne/components/header/nav/HeaderNavBrand.vue
+++ apps/inselbuehne/components/header/nav/HeaderNavBrand.vue
@@ -1,5 +1,5 @@
 <template>
     <NuxtLink to="/">
-        <h1 class="font-serif text-3xl p-2 m-6 whitespace-nowrap">Inselbühne Potsdam</h1>
+        <h1 class="font-serif text-3xl px-4 lg:px-2 xl:px-4 py-2 -ml-4 my-6 lg:m-6 whitespace-nowrap">Inselbühne Potsdam</h1>
     </NuxtLink>
 </template>
