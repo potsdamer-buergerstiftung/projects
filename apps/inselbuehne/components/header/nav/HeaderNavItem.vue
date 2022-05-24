f1d7a6732cc1abf4eef011b187ae3c83d735d004
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-24 11:17:42 +0200

    change style of active header item

--- apps/inselbuehne/components/header/nav/HeaderNavItem.vue
+++ apps/inselbuehne/components/header/nav/HeaderNavItem.vue
@@ -1,7 +1,6 @@
 <template>
-    <NuxtLink :to="link"
-        class="relative hover:after:opacity-100 px-2 py-3 lg:p-2 text-xl lg:text-lg lg:after:bg-black lg:after:absolute lg:after:bottom-0 lg:after:block lg:after:h-[1.5px] lg:after:right-6 lg:after:left-6 lg:after:opacity-0 lg:after:transition"
-        active-class="text-green-500 lg:after:opacity-100 lg:text-inherit" @click="setMobileMenu()">
+    <NuxtLink :to="link" class="relative px-2 py-3 text-xl transition lg:p-2 lg:text-lg hover:text-green-500"
+        active-class="text-green-500" @click="setMobileMenu()">
         {{ title }}
     </NuxtLink>
 </template>
