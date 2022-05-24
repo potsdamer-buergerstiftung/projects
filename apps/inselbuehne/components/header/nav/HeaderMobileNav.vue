3440ef7b9b6452dda47632ef654fb666baf89cb5
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-24 11:49:44 +0200

    margin to padd

--- apps/inselbuehne/components/header/nav/HeaderMobileNav.vue
+++ apps/inselbuehne/components/header/nav/HeaderMobileNav.vue
@@ -2,7 +2,7 @@
     <div v-if="mdAndSmaller"
         class="fixed top-0 bottom-0 left-0 right-0 z-30 overflow-hidden transition duration-500 ease-in-out -translate-x-full bg-white overscroll-contain lg:hidden"
         :class="{ 'translate-x-0': headerState.isMobileMenuOpen }">
-        <div class="container flex flex-col px-2 mx-auto mt-44">
+        <div class="container flex flex-col px-2 mx-auto pt-44">
             <HeaderNavItem title="Veranstaltungen" link="/veranstaltungen" />
             <HeaderNavItem title="Blog" link="/blog" />
             <HeaderNavItem title="Über uns" link="/ueber-uns" />
