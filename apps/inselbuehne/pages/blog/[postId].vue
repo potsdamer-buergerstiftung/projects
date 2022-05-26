df3a849cf773ca59b7c2f18a61fc78de35aa898e
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-26 02:40:06 +0200

    add page for single post

--- apps/inselbuehne/pages/blog/[postId].vue
+++ apps/inselbuehne/pages/blog/[postId].vue
@@ -1,5 +1,12 @@
 <template>
-    <h1 class="pt-64">{{ post.title }}</h1>
+    <div class="container px-4 mx-auto">
+        <h1 class="font-serif text-5xl text-center pt-64 mb-16">{{ post.title }}</h1>
+        <p>{{ post.date }}</p>
+        <ClientOnly>
+            <p v-html="post.content" class="prose lg:prose-xl text-center" />
+        </ClientOnly>
+        <p class="text-2xl font-bold">{{ post.user_created.email.split("@")[0] }}</p>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -12,5 +19,5 @@ const route = useRoute()
 
 const { getItemById } = useDirectusItems();
 
-const post = await getItemById({ collection: "posts", id: route.params.postId as string }) as any
+const post = await getItemById({ collection: "posts", id: route.params.postId as string, params: { fields: ["*.*"] } }) as any
 </script>
