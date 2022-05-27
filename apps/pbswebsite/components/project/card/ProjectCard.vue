e081e8e9d18032c752f374050fdc743642b4bbf4
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-27 16:33:18 +0200

    add custom grid layout to projects

--- apps/pbswebsite/components/project/card/ProjectCard.vue
+++ apps/pbswebsite/components/project/card/ProjectCard.vue
@@ -1,22 +1,23 @@
 <template>
     <NuxtLink :to="`/projekte/${projectId}`"
         class="block relative w-full overflow-hidden rounded-lg cursor-pointer group h-96">
-        <img :src="`https://cms.potsdamer-buergerstiftung.org/assets/${imageId}?width=600&height=600&quality=40`"
-            class="object-cover transition duration-500 group-hover:scale-110" />
+        <img :src="`https://cms.potsdamer-buergerstiftung.org/assets/${imageId}?${imageProps}&quality=40`"
+            class="w-full h-full object-cover transition duration-500 group-hover:scale-110" />
         <div
-            class="absolute top-0 bottom-0 left-0 right-0 pointer-events-none bg-gradient-to-t from-black opacity-60" />
+            class="absolute top-0 bottom-0 left-0 right-0 pointer-events-none bg-gradient-to-t from-black via-transparent to-transparent opacity-80" />
         <div class="absolute top-0 bottom-0 left-0 right-0 flex items-end p-8 flex-column">
             <div class="relative w-full">
-                <h1 class="text-2xl font-bold text-white">{{ title }}</h1>
+                <h1 class="text-2xl font-bold text-white font-header">{{ title }}</h1>
                 <div class="relative w-full mt-2 overflow-hidden">
                     <p
                         class="block text-white opacity-100 transition duration-500 translate-y-0 group-hover:-translate-y-3 group-hover:opacity-0">
                         {{ subTitle }}</p>
                     <div
                         class="absolute text-white opacity-0 transition duration-500 translate-y-0 group-hover:-translate-y-full group-hover:opacity-100">
-                        <NuxtLink to="/" class="relative font-bold">
-                            Projekt anzeigen
-                        </NuxtLink>
+                        <div class="relative font-bold flex flex-row items-center">
+                            <span>Projekt anzeigen</span>
+                            <div class="w-12 h-0.5 bg-white ml-2" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -25,5 +26,7 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ title: string, imageId: string, subTitle: string, projectId: string }>()
+const { imageSize = "small" } = defineProps<{ title: string, imageId: string, subTitle: string, projectId: string, imageSize?: "small" | "large" }>()
+
+const imageProps = imageSize == "small" ? "width=500&height=500" : "width=1100&height=900";
 </script>
--- apps/pbswebsite/pages/index.vue
+++ apps/pbswebsite/pages/index.vue
@@ -1,18 +1,25 @@
 <template>
-    <div class="container px-4 pt-20 mx-auto">
-        <h1 class="font-bold font-header text-7xl text-navy-900">Brücken bauen,<br /> Menschen verbinden</h1>
-        <p class="mt-8 text-navy-900">Wir sind die Potsdamer Bürgerstiftung und fördern als Mitmach-Stiftung
-            ehrenamtliches
-            Engagement.</p>
-        <p class="pt-1 font-bold text-green-500">Lass uns gemeinsam unsere schöne Stadt noch l(i)ebenswerter machen.
-        </p>
-    </div>
-    <div class="container px-4 pt-20 mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8">
-            <div v-for="project in projects">
-                <ProjectCard :title="project.title" :sub-title="project.sub_title" :project-id="project.id"
-                    :image-id="project.image" />
+    <div>
+        <div class="container px-4 pt-20 mx-auto">
+            <h1 class="font-bold font-header text-7xl text-navy-900">Brücken bauen,<br /> Menschen verbinden</h1>
+            <p class="mt-8 text-navy-900">Wir sind die Potsdamer Bürgerstiftung und fördern als Mitmach-Stiftung
+                ehrenamtliches
+                Engagement.</p>
+            <p class="pt-1 font-bold text-green-500">Lass uns gemeinsam unsere schöne Stadt noch l(i)ebenswerter machen.
+            </p>
+        </div>
+        <div class="container px-4 pt-20 mx-auto pb-16">
+            <div class="grid grid-cols-6 gap-8">
+                <div v-for="(project, i) in projects" class="col-span-6 lg:col-span-3"
+                    :class="{ 'xl:col-span-2': indexToColSpan(i) * 2 == 2, 'xl:col-span-4': indexToColSpan(i) * 2 == 4 }">
+                    <ProjectCard :title="project.title" :sub-title="project.sub_title" :project-id="project.id"
+                        :image-id="project.image" :image-size="indexToColSpan(i) * 2 == 2 ? 'small' : 'large'" />
+                </div>
             </div>
+            <button
+                class="mt-8 bg-green-100 text-navy-900 py-1.5 px-4 font-header font-bold text-md rounded-md transition ease-in-out hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
+                Alle Projekte
+            </button>
         </div>
     </div>
 </template>
@@ -25,5 +32,12 @@ definePageMeta({
 
 const { getItems } = useDirectusItems();
 
-const projects = await getItems<any>({ collection: "projects" })
+const projects = await getItems<any>({
+    collection: "projects", params: {
+        sort: "sort",
+        limit: 7
+    }
+})
+
+const indexToColSpan = useGridSpanLayout()
 </script>
