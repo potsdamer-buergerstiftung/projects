1ab002e45232938aed1af9b01ad7dd199831d46c
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-30 01:39:48 +0200

    add single post page

--- apps/pbswebsite/components/article/ArticleCard.vue
+++ apps/pbswebsite/components/article/ArticleCard.vue
@@ -1,14 +1,14 @@
 <template>
     <NuxtLink :to="`/aktuelles/${postId}`" class="group block w-full overflow-hidden rounded-md">
         <div class="relative aspect-[9/10] overflow-hidden">
-            <img class="h-full w-full object-cover"
+            <img class="h-full w-full object-cover transition duration-500 group-hover:scale-110"
                 :src="`https://cms.potsdamer-buergerstiftung.org/assets/${imageId}?$width=700&quality=40`" />
             <div
-                class="opacity-0 translate-y-2 group-hover:translate-y-0 p-6 absolute left-0 right-0 bottom-0 group-hover:opacity-100 bg-gradient-to-t from-slate-900 to-transparent transition duration-500">
-                <div class="items-center inline-flex font-bold text-white">Beitrag aufrufen <svg xmlns="http://www.w3.org/2000/svg"
-                        class="ml-1 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                    </svg></div>
+                class="opacity-0 translate-y-3 group-hover:translate-y-0 p-6 absolute left-0 right-0 bottom-0 group-hover:opacity-100 bg-gradient-to-t from-slate-900 to-transparent transition duration-500">
+                <div class="relative flex flex-row items-center font-bold">
+                    <span class="text-white">Beitrag aufrufen</span>
+                    <div class="ml-2 h-0.5 w-12 bg-white" />
+                </div>
             </div>
         </div>
         <div class="bg-slate-100 px-6 py-8">
---
+++ apps/pbswebsite/pages/aktuelles/[postId].vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <section class="relative">
+            <div class="absolute w-full h-full">
+                <img class="w-full h-full object-cover"
+                    :src="`https://cms.potsdamer-buergerstiftung.org/assets/${post.image}`" />
+            </div>
+            <div class="absolute top-0 left-0 bottom-0 right-0 bg-slate-900 opacity-70" />
+            <div class="relative mx-auto max-w-5xl px-4 pt-40">
+                <h1 class="font-header font-bold text-7xl text-white">{{ post.title }}</h1>
+                <div class="flex flex-row gap-8 mt-8">
+                    <div>
+                        <p class="text-slate-300">Autor</p>
+                        <p class="text-white font-bold">{{ post.user_created.first_name }} {{
+                                post.user_created.last_name
+                        }}</p>
+                    </div>
+                    <div>
+                        <p class="text-slate-300">Veröffentlicht</p>
+                        <p class="text-white font-bold">{{ formattedDate }}</p>
+                    </div>
+                    <div>
+                        <p class="text-slate-300">Diskussion</p>
+                        <p class="text-white font-bold">Keine Kommentare</p>
+                    </div>
+                </div>
+                <PageTitleBreadcrumb :items="[{ text: 'Aktuelles', to: '/aktuelles' }, { text: post.title }]"
+                    class="mt-20 pb-12 text-white" />
+            </div>
+        </section>
+        <section>
+            <div class="mx-auto max-w-5xl px-4 pt-12 pb-20">
+                <div class="prose lg:prose-lg max-w-full" v-html="post.content" />
+            </div>
+        </section>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { getItemById } = useDirectusItems();
+
+const route = useRoute();
+
+const { date, ...post } = await getItemById<any>({
+    collection: "posts",
+    id: route.params.postId as string,
+    params: {
+        fields: ["*", "user_created.*"]
+    }
+});
+
+const formattedDate = computed(() => new Date(date).toLocaleDateString("de", {
+    year: "numeric",
+    day: "numeric",
+    month: "long",
+}));
+
+definePageMeta({
+    headerColor: "white"
+})
+</script>
