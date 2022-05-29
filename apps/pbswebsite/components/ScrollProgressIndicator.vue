7ad960003afb94c76d318d630de256da39e40c96
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-30 00:55:59 +0200

    let progress indicator be green on hover

--- apps/pbswebsite/components/ScrollProgressIndicator.vue
+++ apps/pbswebsite/components/ScrollProgressIndicator.vue
@@ -1,18 +1,12 @@
 <template>
-    <button
-        class="flex flex-row items-center space-x-4 opacity-100 transition duration-500"
-        @click="scrollToTop"
-        :class="{ 'opacity-0': progress < 20 }"
-    >
+    <button class="group flex flex-row items-center space-x-4 opacity-100 transition duration-500" @click="scrollToTop"
+        :class="{ 'opacity-0': progress < 20 }">
         <div class="relative h-[2px] w-14 rotate-180 bg-gray-200">
-            <div
-                class="absolute h-full bg-slate-900"
-                v-bind:style="{
-                    width: progress + '%',
-                }"
-            />
+            <div class="absolute h-full bg-slate-900 group-hover:bg-emerald-500 transition" v-bind:style="{
+                width: progress + '%',
+            }" />
         </div>
-        <p class="text-sm font-bold transition hover:text-green-500">Nach oben scrollen</p>
+        <p class="text-sm font-bold transition group-hover:text-green-500">Zurück nach oben</p>
     </button>
 </template>
 
