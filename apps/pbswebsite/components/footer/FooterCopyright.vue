75b3acea7d3509ebf1e20f74ba33fe34acef56c8
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-30 20:37:51 +0200

    add nice names to footer

--- apps/pbswebsite/components/footer/FooterCopyright.vue
+++ apps/pbswebsite/components/footer/FooterCopyright.vue
@@ -4,17 +4,13 @@ const year = new Date().getFullYear();
 
 <template>
     <div class="container mx-auto grid grid-cols-1 justify-between gap-4 px-4 py-6 md:grid-cols-2">
-        <p>© {{ year }} Potsdamer Bürgerstiftung und Mitwirkende</p>
+        <p>© {{ year }} Potsdamer Bürgerstiftung - By Nöl & Lotte</p>
         <ul class="flex flex-row justify-start space-x-4 md:justify-end">
             <li>
-                <NuxtLink to="/impressum" class="text-navy-900 transition hover:text-green-500"
-                    >Impressum</NuxtLink
-                >
+                <NuxtLink to="/impressum" class="text-navy-900 transition hover:text-green-500">Impressum</NuxtLink>
             </li>
             <li>
-                <NuxtLink to="/datenschutz" class="text-navy-900 transition hover:text-green-500"
-                    >Datenschutz</NuxtLink
-                >
+                <NuxtLink to="/datenschutz" class="text-navy-900 transition hover:text-green-500">Datenschutz</NuxtLink>
             </li>
         </ul>
     </div>
