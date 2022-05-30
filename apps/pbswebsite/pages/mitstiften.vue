6efd8e6e64febf1ea676b771cf7167b41eaa2ca0
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-30 14:53:59 +0200

    fix gap vertical

--- apps/pbswebsite/pages/mitstiften.vue
+++ apps/pbswebsite/pages/mitstiften.vue
@@ -10,7 +10,7 @@
             </template>
         </PageTitle>
         <section class="bg-white">
-            <div class="container mx-auto grid grid-cols-8 gap-8 px-4 pt-8 pb-16">
+            <div class="container mx-auto grid grid-cols-8 gap-x-8 gap-y-12 px-4 pt-8 pb-16">
                 <div class="col-span-8 lg:col-span-2">
                     <h1 class="font-header text-3xl font-bold">Spende</h1>
                     <p class="mt-3">Mit Ihrer Spende die Zukunft Ihrer Stadt formen</p>
@@ -25,24 +25,13 @@
                         Spenden Sie mit einem <b>von Ihnen bestimmten Spendenzweck</b> oder
                         unterstützen Sie die allgemeine Arbeit der Bürgerstiftung.
                     </p>
-                    <NuxtLink
-                        to="/mitstiften/freundeskreis"
-                        class="group mt-6 inline-flex items-center rounded-md text-base font-bold text-slate-900 transition hover:text-emerald-500"
-                    >
+                    <NuxtLink to="/mitstiften/freundeskreis"
+                        class="group mt-6 inline-flex items-center rounded-md text-base font-bold text-slate-900 transition hover:text-emerald-500">
                         Mehr erfahren
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
+                        <svg xmlns="http://www.w3.org/2000/svg"
                             class="ml-1 h-5 w-5 transition group-hover:translate-x-1 group-hover:text-emerald-500"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M17 8l4 4m0 0l-4 4m4-4H3"
-                            />
+                            fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                         </svg>
                     </NuxtLink>
                 </div>
@@ -58,18 +47,15 @@
                     </h4>
                     <div class="mt-4 flex flex-col gap-y-4 gap-x-2 md:flex-row">
                         <button
-                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-blue-800 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75"
-                        >
+                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-blue-800 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
                             PayPal
                         </button>
                         <button
-                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-slate-900 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75"
-                        >
+                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-slate-900 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
                             Kreditkarte
                         </button>
                         <button
-                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-rose-300 py-1.5 px-4 text-center font-bold text-slate-900 transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75"
-                        >
+                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-rose-300 py-1.5 px-4 text-center font-bold text-slate-900 transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
                             Klarna
                         </button>
                     </div>
@@ -77,7 +63,7 @@
             </div>
         </section>
         <section class="bg-slate-100">
-            <div class="container mx-auto grid grid-cols-8 gap-8 px-4 py-16">
+            <div class="container mx-auto grid grid-cols-8 gap-x-8 gap-y-12 px-4 py-16">
                 <div class="col-span-8 lg:col-span-2">
                     <h1 class="font-header text-3xl font-bold">Freundeskreis</h1>
                     <p class="mt-3">
@@ -92,24 +78,13 @@
                         <b>regelmäßigen und verlässlichen Unterstützung</b> können wir diese
                         wichtigen Voraussetzungen schaffen.
                     </p>
-                    <NuxtLink
-                        to="/mitstiften/freundeskreis"
-                        class="group mt-6 inline-flex items-center rounded-md text-base font-bold text-slate-900 transition hover:text-emerald-500"
-                    >
+                    <NuxtLink to="/mitstiften/freundeskreis"
+                        class="group mt-6 inline-flex items-center rounded-md text-base font-bold text-slate-900 transition hover:text-emerald-500">
                         Mehr erfahren
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
+                        <svg xmlns="http://www.w3.org/2000/svg"
                             class="ml-1 h-5 w-5 transition group-hover:translate-x-1 group-hover:text-emerald-500"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M17 8l4 4m0 0l-4 4m4-4H3"
-                            />
+                            fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                         </svg>
                     </NuxtLink>
                 </div>
@@ -126,18 +101,15 @@
                     </h4>
                     <div class="mt-4 flex flex-col gap-y-4 gap-x-2 md:flex-row">
                         <button
-                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-blue-800 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75"
-                        >
+                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-blue-800 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
                             PayPal
                         </button>
                         <button
-                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-slate-900 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75"
-                        >
+                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-slate-900 py-1.5 px-4 text-center font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
                             Kreditkarte
                         </button>
                         <button
-                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-rose-300 py-1.5 px-4 text-center font-bold text-slate-900 transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75"
-                        >
+                            class="text-md font-header inline-flex w-full items-center justify-center rounded-md bg-rose-300 py-1.5 px-4 text-center font-bold text-slate-900 transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
                             Klarna
                         </button>
                     </div>
@@ -145,7 +117,7 @@
             </div>
         </section>
         <section class="bg-white">
-            <div class="container mx-auto grid grid-cols-8 gap-8 px-4 py-16">
+            <div class="container mx-auto grid grid-cols-8 gap-x-8 gap-y-12 px-4 py-16">
                 <div class="col-span-8 lg:col-span-2">
                     <h1 class="font-header text-3xl font-bold">Firmenspende</h1>
                     <p class="mt-3">
@@ -161,24 +133,13 @@
                         Gründe, als <b>Potsdamer Unternehmen</b> die Arbeit der Bürgerstiftung zu
                         fördern.
                     </p>
-                    <NuxtLink
-                        to="/mitstiften/freundeskreis"
-                        class="group mt-6 inline-flex items-center rounded-md text-base font-bold text-slate-900 transition hover:text-emerald-500"
-                    >
+                    <NuxtLink to="/mitstiften/freundeskreis"
+                        class="group mt-6 inline-flex items-center rounded-md text-base font-bold text-slate-900 transition hover:text-emerald-500">
                         Mehr erfahren
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
+                        <svg xmlns="http://www.w3.org/2000/svg"
                             class="ml-1 h-5 w-5 transition group-hover:translate-x-1 group-hover:text-emerald-500"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M17 8l4 4m0 0l-4 4m4-4H3"
-                            />
+                            fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                         </svg>
                     </NuxtLink>
                 </div>
@@ -189,24 +150,13 @@
                         <b>Unterstützen Sie mit uns Ihre Projektidee</b> oder finden Sie mit unseren
                         Projekten ein <b>passendes Engagement</b> für Ihr Unternehmen.
                     </p>
-                    <NuxtLink
-                        to="/mitstiften"
-                        class="text-md font-header mt-6 inline-flex items-center rounded-md bg-slate-900 py-1.5 px-4 font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75"
-                    >
+                    <NuxtLink to="/mitstiften"
+                        class="text-md font-header mt-6 inline-flex items-center rounded-md bg-slate-900 py-1.5 px-4 font-bold text-white transition ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
                         Sprechen Sie uns an
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            class="ml-1 h-4 w-4"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            stroke-width="2"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                            />
+                        <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                         </svg>
                     </NuxtLink>
                 </div>
