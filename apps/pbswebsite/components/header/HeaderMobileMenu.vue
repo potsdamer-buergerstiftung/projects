7b05374ffcf2ccfc809bb2c6db30dfb806c55cf7
Nöl <65312133+linlogge@users.noreply.github.com>
2022-05-30 13:17:14 +0200

    use cdn cache for image

--- apps/pbswebsite/components/header/HeaderMobileMenu.vue
+++ apps/pbswebsite/components/header/HeaderMobileMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-0 left-0 right-0 bottom-0 z-50 h-screen overflow-y-hidden bg-slate-900">
-        <div class="flex h-screen flex-col items-start gap-10 overflow-y-scroll px-4 py-6">
+        <div class="container mx-auto flex h-screen flex-col items-start gap-10 overflow-y-scroll px-4 py-6">
             <HeaderMobileMenuButton variant="close" />
             <HeaderNav class="-ml-4" />
             <HeaderActions />
@@ -8,7 +8,3 @@
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-const headerState = useHeaderState();
-</script>
--- apps/pbswebsite/pages/mitstiften.vue
+++ apps/pbswebsite/pages/mitstiften.vue
@@ -9,5 +9,29 @@
                 <PageTitleBreadcrumb :items="[{ text: 'Mitstiften' }]" />
             </template>
         </PageTitle>
+        <section class="bg-slate-100">
+            <div class="container mx-auto grid grid-cols-4 gap-8 px-4 py-20">
+                <div class="col-span-4 lg:col-span-1">
+                    <h1 class="font-header text-3xl font-bold">Unsere Standorte</h1>
+                    <p class="mt-3">Komm einfach vorbei</p>
+                </div>
+                <div class="col-span-4 md:col-span-2 lg:col-span-1">
+                    <h2 class="font-header text-xl font-bold">MAZ-Pyramide</h2>
+                    <p class="mt-4">Friedrich-Engels-Straße 24</p>
+                    <p>14472 Potsdam</p>
+                    <p class="mt-1 font-bold">+49 331 23180300</p>
+                    <p class="mt-4">Arbeitszeiten: Montag - Freitag</p>
+                    <p>9 Uhr - 15 Uhr</p>
+                </div>
+                <div class="col-span-4 md:col-span-2 lg:col-span-1">
+                    <h2 class="font-header text-xl font-bold">Inselbühne</h2>
+                    <p class="mt-4">Burgstraße</p>
+                    <p>14467 Potsdam</p>
+                    <p class="mt-1 font-bold">+49 331 23180309</p>
+                    <p class="mt-4">Besuchszeiten: Freitag - Samstag</p>
+                    <p>18 Uhr - 20 Uhr</p>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
