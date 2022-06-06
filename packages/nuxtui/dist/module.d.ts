7e7b6cd6bcffa7b4948ab644ee9c74dbf242b4a1
Nöl <65312133+linlogge@users.noreply.github.com>
2022-06-06 19:01:07 +0200

    delete dist

--- packages/nuxtui/dist/module.d.ts
+++
@@ -1,2 +0,0 @@
-export * from '/Users/noelsigmunczyk/Desktop/pbsprojects/packages/nuxtui/src/module';
-export { default } from '/Users/noelsigmunczyk/Desktop/pbsprojects/packages/nuxtui/src/module';
--- packages/nuxtui/dist/module.mjs
+++
@@ -1,2 +0,0 @@
-import jiti from 'jiti';
-export default jiti(null, { interopDefault: true })('/Users/noelsigmunczyk/Desktop/pbsprojects/packages/nuxtui/src/module');
