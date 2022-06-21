7a0f9992fd227a9b5671acd8c03c966f25df28fe
Nöl <65312133+linlogge@users.noreply.github.com>
2022-06-21 17:40:49 +0200

    delete portal repo

--- apps/portal/app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="w-24">
-    <PbsLogo />
-    <div class="max-w-24">
-      <p class="line-clamp-3 font-header">Hallo ich bin ein toller Mensch</p>
-    </div>
-  </div>
-</template>
