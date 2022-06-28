8c8d8cb51b0e85bc5ae027f94e51775c1e260ed3
Nöl <65312133+linlogge@users.noreply.github.com>
2022-06-28 12:26:55 +0200

    remove analytics

--- apps/inselbuehne/layouts/default.vue
+++ apps/inselbuehne/layouts/default.vue
@@ -4,12 +4,6 @@ const route = useRoute();
 
 <template>
   <div class="antialiased">
-    <Script
-      async
-      defer
-      data-website-id="54874799-7dc4-407f-8ee6-ea2d08dc4cf1"
-      src="https://analytics.potsdamer-buergerstiftung.org/umami.js"
-    ></Script>
     <Header />
     <slot />
     <Footer />
--- apps/pbswebsite/app.vue
+++ apps/pbswebsite/app.vue
@@ -1,10 +1,4 @@
 <template>
-  <Script
-    async
-    defer
-    data-website-id="1b0d1b14-b2bd-4d55-bba7-103f92c44166"
-    src="https://analytics.potsdamer-buergerstiftung.org/umami.js"
-  />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
