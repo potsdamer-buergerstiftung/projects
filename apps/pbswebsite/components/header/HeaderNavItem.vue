fc2d13a086f972a2b5cf66a7015289fed11703ed
Nöl <65312133+linlogge@users.noreply.github.com>
2022-07-10 18:21:17 +0200

    reduce padding

--- apps/pbswebsite/components/header/HeaderNavItem.vue
+++ apps/pbswebsite/components/header/HeaderNavItem.vue
@@ -2,7 +2,7 @@
   <div @mouseenter="onHover()" @mouseleave="onLeave()" ref="node" @click="onClick()"
     class="group relative flex flex-col justify-start">
     <NuxtLink
-      class="group flex flex-row justify-start gap-1 px-4 py-4 outline-none lg:flex-col lg:gap-0 lg:px-3 lg:py-2 xl:px-4"
+      class="group flex flex-row justify-start gap-1 px-4 py-4 outline-none lg:flex-col lg:gap-0 lg:px-3 lg:py-2"
       :to="link">
       <span
         class="text-xs font-bold leading-4 transition group-hover:text-emerald-500 group-focus:text-emerald-500" :class="[isDark ? 'text-slate-400' : 'text-slate-500']">{{
@@ -12,7 +12,7 @@
         class="text-2xl font-bold text-white transition group-hover:text-emerald-500 group-focus:text-emerald-500 lg:text-[1rem] lg:font-medium lg:leading-6"
         :class="[isDark ? 'lg:text-white' : 'lg:text-slate-900']">{{ title }}</span>
     </NuxtLink>
-    <ul class="relative mb-4 w-auto flex-col whitespace-nowrap px-9 lg:absolute lg:mb-0 lg:px-4 lg:py-20" ref="subMenu"
+    <ul class="relative mb-4 w-auto flex-col whitespace-nowrap px-9 lg:absolute lg:mb-0 lg:px-3 lg:py-20" ref="subMenu"
       :class="[store.isSubMenuOpen && itemSubMenuOpen ? 'flex' : 'hidden']" v-if="$slots.default">
       <slot />
     </ul>
