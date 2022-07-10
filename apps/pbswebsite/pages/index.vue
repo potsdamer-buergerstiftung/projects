3e54e127045966a434cbc4be1d383b0c873b205d
Nöl <65312133+linlogge@users.noreply.github.com>
2022-07-10 18:21:39 +0200

    add priorities section

--- apps/pbswebsite/pages/index.vue
+++ apps/pbswebsite/pages/index.vue
@@ -57,6 +57,47 @@
         </div>
       </div>
     </section>
+    <section>
+      <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 overflow-hidden">
+        <div class="relative" v-for="priority in priorities">
+          <div class="z-[-1] absolute h-full w-full">
+            <div class="absolute bottom-0 top-0 left-0 right-0 bg-slate-900 opacity-60" />
+            <DirectusImage :asset-id="priority.assetId" class="object-cover w-full h-full" :width="500" :height="700"
+              :quality="30" v-if="priority.assetId" />
+          </div>
+          <div class="container mx-auto px-4 lg:max-w-none py-16 lg:py-8 lg:px-8 flex flex-col justify-between h-full">
+            <div class="mb-36">
+              <h1 class="text-sm font-semibold uppercase text-slate-200 mb-1">{{ priority.subTitle }}</h1>
+              <h2 class="text-white font-header font-bold text-3xl">{{ priority.title }}</h2>
+            </div>
+            <div>
+              <p class="text-white">{{ priority.description }}</p>
+            </div>
+          </div>
+        </div>
+        <div class="relative">
+          <div class="z-[-1] absolute h-full w-full">
+            <div class="absolute bottom-0 top-0 left-0 right-0 bg-slate-900" />
+          </div>
+          <div class="container mx-auto px-4 lg:max-w-none py-16 lg:py-8 lg:px-8 flex flex-col justify-end h-full">
+            <div>
+              <NuxtLink to="/mitstiften"
+                class="mb-6 text-md font-header inline-flex items-center rounded-md text-emerald-500 font-bold transition ease-in-out hover:text-emerald-200">
+                Unterstütze uns
+                <svg xmlns="http://www.w3.org/2000/svg" class="ml-1 h-4 w-4" fill="none" viewBox="0 0 24 24"
+                  stroke="currentColor" stroke-width="2">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                </svg>
+              </NuxtLink>
+              <p class="text-white">
+                Ermögliche mit Deiner Spende und einem von Dir bestimmten Spendenzweck, welche unserer
+                Projekte und Projektkategorien Du unterstützen möchtest.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
     <section class="py-24">
       <div class="container mx-auto grid gap-8 px-4 grid-cols-6">
         <div class="col-span-6 lg:col-span-4">
@@ -66,8 +107,8 @@
           <h1 class="font-header mt-2 text-4xl font-bold">
             Aktuelles & Neuigkeiten
           </h1>
-          <p class="mt-4 max-w-2xl">Verfolgen Sie die Entwicklung unserer Projekte, die dank Ihrer großzügigen Beiträge
-            und ihrer Unterstützung möglich wurden.</p>
+          <p class="mt-4 max-w-2xl">Verfolge die Entwicklung unserer Projekte, die dank Deiner großzügigen Beiträge
+            und Deiner Unterstützung möglich wurden.</p>
           <NuxtLink to="/stiftung/aktuelles"
             class="text-md font-header mt-12 mb-4 inline-flex items-center rounded-md bg-green-100 py-1.5 px-4 font-bold transition ease-in-out hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75">
             Alle Beiträge
@@ -91,6 +132,26 @@
 definePageMeta({
   layout: "default",
 });
+
+const priorities = [{
+  subTitle: 'Nachhaltigkeit',
+  title: 'Nachhaltig engagieren und handeln',
+  assetId: '0e823ed4-1409-4827-b9c3-14a442a1db52',
+  description: `Wir verschaffen Nachhaltigkeit und Müllvermeidung in Potsdam ein größeres Gehör und unterstützen lokale Unternehmen dabei, sich nachhaltig entwickeln zu können.`,
+},
+{
+  subTitle: 'Kultur',
+  title: 'Chancengleichheit und Zugang zu Kultur schaffen',
+  assetId: '72baf604-4397-4f22-9ac7-195df8b1a591',
+  description: `Finanzierbarer Zugang zu Kultur und Unterhaltung ist besonders wegen der Pandemiemaßnahmen wichtiger denn je. Wir ermöglichen Kunstschaffenen sich in Potsdam zu präsentieren und jedem, Teil des Publikums zu sein.`,
+},
+{
+  subTitle: 'Begegnung & Toleranz',
+  title: 'Menschen aus aller Welt zusammenbringen',
+  assetId: 'db74ab6f-0e47-415d-8686-a3a6afa2b6a1',
+  description: `Menschen in Potsdam kennenlernen, zusammen leben und Spaß haben. Wir organisieren Zusammentreffen aller Art und vereinfachen Begegnung durch gemeinsame Interessen.`,
+},
+];
 
 const { getItems } = useDirectusItems();
 
