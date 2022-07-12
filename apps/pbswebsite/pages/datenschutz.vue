718396ad36f7c63732af353fa1eb4ff65eb732de
Nöl <65312133+linlogge@users.noreply.github.com>
2022-07-12 14:07:16 +0200

    add privacy policy and imprint

--- apps/pbswebsite/pages/datenschutz.vue
+++ apps/pbswebsite/pages/datenschutz.vue
@@ -1,4 +1,21 @@
 <template>
-  <h1>Datenschutz</h1>
-  <h2>Server</h2>
+  <div>
+    <Title>Datenschutz</Title>
+    <PageTitle title="Datenschutz" class="max-w-4xl mx-auto -mt-24">
+      <template #breadcrumb>
+        <PageTitleBreadcrumb :items="[{ text: 'Datenschutz', link: '/datenschutz' }]" />
+      </template>
+    </PageTitle>
+    <section class="pb-20 max-w-4xl mx-auto px-4">
+      <BlockRenderer :data="policy.content" />
+    </section>
+  </div>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "text"
+})
+const { getSingletonItem } = useDirectusItems()
+const policy = await getSingletonItem<any>({ collection: "privacy_policy" })
+</script>
--- apps/pbswebsite/pages/impressum.vue
+++ apps/pbswebsite/pages/impressum.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Impressum</h1>
+  <div>
+    <Title>Impressum</Title>
+    <PageTitle title="Impressum" class="max-w-4xl mx-auto -mt-24">
+      <template #breadcrumb>
+        <PageTitleBreadcrumb :items="[{ text: 'Impressum' }]" />
+      </template>
+    </PageTitle>
+    <section class="pb-20 max-w-4xl mx-auto px-4">
+      <BlockRenderer :data="imprint.content" />
+    </section>
+  </div>
 </template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: "text"
+})
+const { getSingletonItem } = useDirectusItems()
+const imprint = await getSingletonItem<any>({ collection: "imprint" })
+</script>
