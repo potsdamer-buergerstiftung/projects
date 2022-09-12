07144a082b3cb757abaffab1aa902f9f48f54d6b
Nöl <65312133+linlogge@users.noreply.github.com>
2022-09-12 18:21:33 +0200

    add images, make close button accessible

--- apps/pbswebsite/components/header/HeaderQuickDonateDrawer.vue
+++ apps/pbswebsite/components/header/HeaderQuickDonateDrawer.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="fixed top-0 right-0 bottom-0 bg-slate-50 z-50 shadow-lg w-2/3 transition duration-500 overflow-y-auto"
         :class="[!store.isQuickDonateDrawerOpen ? 'translate-x-full' : 'translate-x-0']">
-        <div class="w-full p-8">
+        <div class="w-full p-8 -mb-12">
             <button @click="store.closeQuickDonateDrawer()"
                 class="block rounded-full p-4 ring-emerald-500 transition hover:ring-1 bg-slate-100 float-right">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-slate-900" fill="none" viewBox="0 0 24 24"
