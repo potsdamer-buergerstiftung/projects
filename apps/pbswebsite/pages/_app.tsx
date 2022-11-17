0e27f4d8014f5a7507bac342de76e724cc8f1a06
Nöl <65312133+linlogge@users.noreply.github.com>
2022-11-17 10:41:55 +0100

    create next app

--- apps/pbswebsite/pages/_app.tsx
+++ apps/pbswebsite/pages/_app.tsx
@@ -1,20 +1,6 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import type { ReactElement, ReactNode } from "react";
-import type { NextPage } from "next";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-
-  return getLayout(<Component {...pageProps} />);
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
