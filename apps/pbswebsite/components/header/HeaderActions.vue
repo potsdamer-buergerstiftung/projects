fd78dadb7ee8c7cfc5e315f824a119ac5f6d1176
Noël Sigmunczyk <65312133+linlogge@users.noreply.github.com>
2023-02-07 20:48:49 +0100

    Disable donation form

--- apps/pbswebsite/components/header/HeaderActions.vue
+++ apps/pbswebsite/components/header/HeaderActions.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="store.openQuickDonateDrawer()" :class="{
+  <NuxtLink href="/mitstiften" :class="{
     'bg-emerald-500 text-white hover:bg-emerald-600':
       route.meta.headerColor === 'white',
     'bg-emerald-100 text-slate-800 hover:bg-emerald-200':
@@ -7,8 +7,8 @@
   }"
     class="text-md font-header inline-flex rounded-md py-1.5 px-4 font-bold transition ease-in-out focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-opacity-75">
     Jetzt spenden
-  </button>
-</template>
+  </a>
+</NuxtLink>
 
 <script setup lang="ts">
 import { useStore } from "./state";
